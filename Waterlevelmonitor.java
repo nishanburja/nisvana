f587c9678769fd714506566fa07dab2e226b9acb
nishanburja <nishanmagar434@gmail.com>
2025-12-02 21:39:43 +0545

    Waterlevelmonitor

---
+++ Waterlevelmonitor.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+/**
+ * Write a description of class Waterlevelmonitor here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Waterlevelmonitor
+{
+    public static void main(String [] args){
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Enter Water level");
+        
+        int level = scanner.nextInt();
+        
+        if (level >= 1000){
+            System.out.println("WARNING! Water level has reached 1000L or more");
+            
+        }
+        else{
+            System.out.println("Status: Normal");
+        }
+    }
+}
